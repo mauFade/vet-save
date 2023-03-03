3497d0556ed776753bb43202209382807c09a20d
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-03 17:43:40 -0300

    config: babel config

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@application": "./src/application",
+          "@infra": "./src/infra",
+          "@domain": "./src/domain",
+          "@configs": "./src/configs",
+        },
+      },
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+  ],
+};
