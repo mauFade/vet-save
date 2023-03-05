6c61734a0567d995bcfcde6eec0f92607b8feeb7
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-05 11:54:33 -0300

    feat: init user services

--- src/application/user/services/create-user.service.ts
+++ src/application/user/services/create-user.service.ts
@@ -1,1 +1,14 @@
-export class CreateuserService {}
+import { UserRepositoryInterface } from "@domain/user/repository/user-interface-repository";
+import { inject, injectable } from "tsyringe";
+
+@injectable()
+export class CreateuserService {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: UserRepositoryInterface
+  ) {}
+
+  public async execute(): Promise<any> {
+    return "a";
+  }
+}
--- src/infra/container/index.ts
+++ src/infra/container/index.ts
@@ -1,1 +1,8 @@
+import { UserRepository } from "@application/user/infra/repositories/user.repository";
+import { UserRepositoryInterface } from "@domain/user/repository/user-interface-repository";
 import { container } from "tsyringe";
+
+container.registerSingleton<UserRepositoryInterface>(
+  "UsersRepository",
+  UserRepository
+);
