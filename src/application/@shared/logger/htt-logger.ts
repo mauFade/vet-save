1758178a9d226db14887597411511af1265c3168
Mauricio Cardoso <mauricio.cds00@gmail.com>
2023-03-05 23:36:37 -0300

    fix: fix logger

--- src/application/@shared/logger/htt-logger.ts
+++ src/application/@shared/logger/htt-logger.ts
@@ -1,10 +1,12 @@
-import { Request, Response, NextFunction } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { NextFunction, Request, Response } from "express";
 import winston from "winston";
 
 import { ErrorInterface } from "../errors";
 import { AppLogger } from "./logger";
 
-type IExpresssReqResLoggerParams = { logger: winston.Logger };
+type IExpressReqResLoggerParams = { logger: winston.Logger };
 
 type IExpressRequestLoggerResponse = (
   req: Request,
@@ -21,43 +23,50 @@ type IExpressErrorLogger = (
 
 type IHttpLogger = {
   req_logger: (
-    param: IExpresssReqResLoggerParams
+    param: IExpressReqResLoggerParams
   ) => IExpressRequestLoggerResponse;
-  err_logger: (param: IExpresssReqResLoggerParams) => IExpressErrorLogger;
+
+  err_logger: (param: IExpressReqResLoggerParams) => IExpressErrorLogger;
 };
 
 function expressRequestLogger(
-  options: IExpresssReqResLoggerParams
+  opts: IExpressReqResLoggerParams
 ): IExpressRequestLoggerResponse {
-  const { logger } = options;
+  const { logger } = opts;
 
   return (req: Request, res: Response, next: NextFunction): void => {
-    function onResDone() {
+    function onResDone(err: Error) {
       res.removeListener("finish", onResDone);
       res.removeListener("error", onResDone);
     }
 
     logger.info(
-      `Handled ${req.method} on ${req.path}. Body: ${JSON.stringify(req.body)}`
+      `handled ${req.method} ${req.path}. body ${JSON.stringify(req.body)}`
     );
 
     res.on("finish", onResDone);
     res.on("error", onResDone);
-
     next();
   };
 }
 
-function expressErrorLogger(): IExpressErrorLogger {
-  return (error: ErrorInterface, req: Request, _res: Response): Response => {
-    AppLogger.error(error);
+function expressErrorLogger(
+  opts: IExpressReqResLoggerParams
+): IExpressErrorLogger {
+  return (
+    err: ErrorInterface,
+    req: Request,
+    _res: Response,
+    _: NextFunction
+  ): Response => {
+    AppLogger.error(err);
 
     return _res
-      .status(error.type === "DomainError" ? 400 : error.status || 500)
+      .status(err.type === "DomainError" ? 400 : err?.status || 500)
       .json({
         error: {
-          errorType: error.type,
-          message: error.message,
+          errorType: err.type,
+          message: err.message,
         },
       });
   };
